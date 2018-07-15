98a91bca38ed7c3c21032ff94746a6b0ac3dd243
shubham <manovagyanik1@gmail.com>
2018-07-15 15:50:58 +0530

    adding header and footer

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,12 @@ let express = require( "express" );
 let router = express.Router();
 
 /* GET home page. */
-router.get( "/*", ( req, res, next ) => {
+
+router.get( "/pricing", ( req, res, next ) => {
+    res.render( "pricing", { appName: "fileIncomeTaxOnline", messenger: "https://m.me/fileincometaxonline" } );
+} );
+
+router.get( "/", ( req, res, next ) => {
     res.render( "index", { appName: "fileIncomeTaxOnline", messenger: "https://m.me/fileincometaxonline" } );
 } );
 
